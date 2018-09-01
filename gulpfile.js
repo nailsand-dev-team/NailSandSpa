46c62abeef0a72b8aa31c845a2ccff0af2a4aca4
Dee Jorillo <dee.jorillo@gmail.com>
2018-09-01 19:57:08 +0800

    Partial incomplete changes for index page

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
 
+
 gulp.task('default', function(){
     browserSync.init({
         server: {
@@ -12,7 +13,7 @@ gulp.task('default', function(){
     gulp.src('./app/images/**/*.jpg')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/images'));
-
+    
     gulp.watch('./app/styles/*.css').on('change', browserSync.reload);
     gulp.watch('./app/index.html').on('change', browserSync.reload);
 });
